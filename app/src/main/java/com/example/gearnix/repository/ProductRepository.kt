fa60bc7726714b1b096ba63e60439cea4d9922f1
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-08-01 12:14:45 +0545

    repositories created

--- app/src/main/java/com/example/gearnix/repository/ProductRepository.kt
+++ app/src/main/java/com/example/gearnix/repository/ProductRepository.kt
@@ -5,11 +5,6 @@ import android.net.Uri
 import com.example.gearnix.model.ProductModel
 
 interface ProductRepository {
-    fun addProduct(model: ProductModel, callback: (Boolean, String) -> Unit)
-    fun deleteProduct(productID: String, callback: (Boolean, String) -> Unit)
-    fun updateProduct(productID: String, productData: MutableMap<String, Any?>, callback: (Boolean, String) -> Unit)
-    fun getProductByID(productID: String, callback: (ProductModel?, Boolean, String) -> Unit)
-    fun getAllProduct(callback: (List<ProductModel?>, Boolean, String) -> Unit)
 
     fun uploadImage(context: Context, imageUri: Uri, callback: (String?) -> Unit)
     fun getFileNameFromUri(context: Context, uri: Uri): String?
--- app/src/main/java/com/example/gearnix/repository/UserRepository.kt
+++ app/src/main/java/com/example/gearnix/repository/UserRepository.kt
@@ -5,13 +5,6 @@ import com.example.gearnix.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
-    fun login(email : String, password : String, callback : (Boolean, String) -> Unit)
-    fun register(email : String, password : String, callback : (Boolean, String, String) -> Unit)
-    fun forgetPassword(email : String, callback : (Boolean, String) ->Unit)
-    fun getCurrentUser() : FirebaseUser?
-    fun addUserToDatabase(userID : String, model: UserModel, callback: (Boolean, String) ->Unit)
-    fun logout(callback : (Boolean, String) ->Unit)
-    fun getUserByID(userID : String, callback : (UserModel?, Boolean, String) ->Unit)
-    fun updateProfile(userID : String, userData: MutableMap<String, Any?>, callback : (Boolean, String) ->Unit)
+    oolean, String) ->Unit)
 //    fun removeData(userID : String, callback: (Boolean, String) -> Unit)
 }
