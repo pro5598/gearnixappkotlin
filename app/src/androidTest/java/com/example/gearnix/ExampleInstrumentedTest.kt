5314f57e9cf27cbf04304fe4f96ad7d1be27cca6
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-08-01 12:18:55 +0545

    Testing completed

--- app/src/androidTest/java/com/example/gearnix/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/gearnix/ExampleInstrumentedTest.kt
@@ -17,6 +17,8 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.gearnix", appContext.packageName)
     }
 }
