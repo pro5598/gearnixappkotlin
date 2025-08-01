f628196644983111a2677d6d76414df9ac312e0a
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-08-01 12:12:10 +0545

    User modal sreated

--- app/src/main/java/com/example/gearnix/model/GamingProduct.kt
+++ app/src/main/java/com/example/gearnix/model/GamingProduct.kt
@@ -2,8 +2,11 @@ package com.example.gearnix.model
 
 data class GamingProduct(
     var id: String = "",
-
-
+    var name: String = "",
+    var category: String = "",
+    var price: String = "",
+    var imageUrl: String = "",
+    var description: String = "",
     var timestamp: Long = System.currentTimeMillis()
 ) {
     // Empty constructor required for Firestore
--- app/src/main/java/com/example/gearnix/model/UserModel.kt
+++ app/src/main/java/com/example/gearnix/model/UserModel.kt
@@ -2,10 +2,5 @@ package com.example.gearnix.model
 
 
 data class UserModel(
-    var userID : String = "",
-    var fullName : String = "",
-    var email : String = "",
-    var password : String = "",
-    var phoneNumber : String = "",
-    var address : String = ""
+
 )
