65067b04122ab6410db6c9f7e917efc638b56d47
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-08-01 12:15:15 +0545

    repositories completed

--- app/src/main/java/com/example/gearnix/repository/UserRepository.kt
+++ app/src/main/java/com/example/gearnix/repository/UserRepository.kt
@@ -5,6 +5,13 @@ import com.example.gearnix.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
-    oolean, String) ->Unit)
+    fun login(email : String, password : String, callback : (Boolean, String) -> Unit)
+    fun register(email : String, password : String, callback : (Boolean, String, String) -> Unit)
+    fun forgetPassword(email : String, callback : (Boolean, String) ->Unit)
+    fun getCurrentUser() : FirebaseUser?
+    fun addUserToDatabase(userID : String, model: UserModel, callback: (Boolean, String) ->Unit)
+    fun logout(callback : (Boolean, String) ->Unit)
+    fun getUserByID(userID : String, callback : (UserModel?, Boolean, String) ->Unit)
+    fun updateProfile(userID : String, userData: MutableMap<String, Any?>, callback : (Boolean, String) ->Unit)
 //    fun removeData(userID : String, callback: (Boolean, String) -> Unit)
 }
