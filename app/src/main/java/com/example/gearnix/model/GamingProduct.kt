f5c33c05eb924722211d7d5a55159858b58fb853
Amrit Bhattarai <amritbhattarai481@gmail.com>
2025-08-01 12:11:20 +0545

    model codes updated

--- app/src/main/java/com/example/gearnix/model/GamingProduct.kt
+++ app/src/main/java/com/example/gearnix/model/GamingProduct.kt
@@ -2,11 +2,8 @@ package com.example.gearnix.model
 
 data class GamingProduct(
     var id: String = "",
-    var name: String = "",
-    var category: String = "",
-    var price: String = "",
-    var imageUrl: String = "",
-    var description: String = "",
+
+
     var timestamp: Long = System.currentTimeMillis()
 ) {
     // Empty constructor required for Firestore
